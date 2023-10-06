65a6a648d64ae0c1a4e28c8669364fff6c13eb59
AndrijaDev <91664827+AndrijaWBDev@users.noreply.github.com>
2023-10-06 10:42:51 +0900

    update search box

---
+++ src/pages/FileManager/index.jsx
@@ -0,0 +1,83 @@
+import NavBar from "@/Layouts/NavBar";
+import FMResizablePanel from "@/Components/FMResizablePanel";
+import FMTreeView from "@/Components/FMTreeView";
+import { useEffect, useState } from "react";
+import SearchBox from "../../Components/FMSearchBox";
+
+const FileManager = () => {
+  const [middlePanelWidth, setMiddlePanelWidth] = useState(300);
+  const [leftPanelWidth, setLeftPanelWidth] = useState(300);
+
+  // Create a function to update the window width when the window is resized
+  const handleWindowResize = () => {
+    setMiddlePanelWidth((window.innerWidth * 5) / 12);
+    setLeftPanelWidth((window.innerWidth * 3) / 12);
+  };
+
+  // Attach the resize event listener when the component mounts
+  useEffect(() => {
+    handleWindowResize();
+    window.addEventListener("resize", handleWindowResize);
+
+    // Remove the event listener when the component unmounts to prevent memory leaks
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+
+  const handleResizwMiddlePanel = (event, { size }) => {
+    setMiddlePanelWidth(size.width);
+  };
+
+  const hadleResizeLeftPanel = (event, { size }) => {
+    setLeftPanelWidth(size.width);
+  };
+
+  return (
+    <div className="">
+      <NavBar />
+      <div className=" mx-9 w-full flex flex-col md:flex-row justify-start mt-28 overflow-y-hidden ">
+        <div className="flex items-start">
+          <img
+            src="/image/favicon.png"
+            alt="Transcribatron.png"
+            className=" mr-2 w-[26px] h-[30px]"
+          />
+          <div className="ml-2 text-[#4489FE] text-[22px]  font-roboto">
+            DeskVantage
+          </div>
+          <div className="ml-1 text-[#3F51B5] text-[22px]  font-roboto">
+            Storage
+          </div>
+        </div>
+        <div className="w-full md:w-calc-full-without-200  md:max-w-[840px] flex items-end ">
+          <SearchBox
+            className={`w-10/12 bg-[#F1F3F4] h-[50px] px-3  mx-0 md:ml-28 `}
+          />
+        </div>
+      </div>
+      <div className="w-full flex relative mt-10">
+        <div className="">
+          <FMResizablePanel
+            width={leftPanelWidth}
+            onResize={hadleResizeLeftPanel}
+          >
+            Resizable Panel2
+            <FMTreeView />
+          </FMResizablePanel>
+        </div>
+
+        <div className="">
+          <FMResizablePanel
+            width={middlePanelWidth}
+            onResize={handleResizwMiddlePanel}
+          >
+            Resizable Panel
+          </FMResizablePanel>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FileManager;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,25 +4,31 @@ const withMT = require("@material-tailwind/react/utils/withMT");
 module.exports = withMT({
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-    screens: {                // customize screen pixel for responsive design
+    extend: {
+      fontFamily: {
+        roboto: ["Roboto", "sans"],
+      },
+      width: {
+        "calc-full-without-200": "calc(100% - 200px)",
+      },
+    },
+    screens: {
+      // customize screen pixel for responsive design
       miniPhone: "360px",
       mobile: "400px",
       tablet: "640px",
       laptop: "1024px",
       desktop: "1280px",
     },
-    fontFamily: {             // define font family
-      sans: ['"Roboto"']      // default font family
-    },
-    colors: {                 // define colors
-      'custom-white': '#FFFBFB',
-      'custom-light-gray': '#DCDCDC',
-      'custom-medium-gray': '#C4C4C4',
-      'custom-gray': '#757575',
-      'custom-black': '#212121',
-      'custom-sky': '#4489FE',
-      'custom-sky-gray': '#F8FAFF'
+    colors: {
+      // define colors
+      "custom-white": "#FFFBFB",
+      "custom-light-gray": "#DCDCDC",
+      "custom-medium-gray": "#C4C4C4",
+      "custom-gray": "#757575",
+      "custom-black": "#212121",
+      "custom-sky": "#4489FE",
+      "custom-sky-gray": "#F8FAFF",
     },
   },
   plugins: [],
