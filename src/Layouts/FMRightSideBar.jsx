02f52f7c67b184350bd9c1e26335f0f8c94f660a
AndrijaDev <91664827+AndrijaWBDev@users.noreply.github.com>
2023-10-07 00:28:25 +0900

    add chat box in the right side panel

--- src/Layouts/FMRightSideBar.jsx
+++ src/Layouts/FMRightSideBar.jsx
@@ -105,11 +105,97 @@ const FMRightSideBar = () => {
                 <div className="text-[#212121] text-lg my-4">
                   Folder Details
                 </div>
+
+                <div className="flex flex-col gap-4">
+                  <div className="w-full flex justify-between">
+                    <div className="text-[#757575]">Owner:</div>
+                    <div className="text-[#212121]">Serhii Movchan</div>
+                  </div>
+
+                  <div className="w-full flex justify-between">
+                    <div className="text-[#757575]">Created:</div>
+                    <div className="text-[#212121]">08/24/2019 05:45 PM</div>
+                  </div>
+
+                  <div className="w-full flex justify-between">
+                    <div className="text-[#757575]">Last Activity</div>
+                    <div className="text-[#212121]">Today 04:15 PM</div>
+                  </div>
+
+                  <div className="w-full flex justify-between">
+                    <div className="text-[#757575]">Total Members</div>
+                    <div className="text-[#212121]">11 Members</div>
+                  </div>
+
+                  <div className="w-full flex justify-between">
+                    <div className="text-[#757575]">Total Files</div>
+                    <div className="text-[#212121]">9 Files</div>
+                  </div>
+
+                  <div className="w-full flex justify-between">
+                    <div className="text-[#757575]">Folder Size</div>
+                    <div className="text-[#212121]">27 MB</div>
+                  </div>
+                </div>
               </div>
             </TabPanel>
 
             <TabPanel key={"chat"} value={"chat"}>
-              {""}
+              <div className="text-[13px] font-bold text-center">Group GPT</div>
+              <div className="text-[11px] font-semibold text-center">
+                8 members, 3 online
+              </div>
+              <div className="flex w-full justify-between relative mt-12">
+                <img
+                  src="/public/image/avatar4.png"
+                  alt=""
+                  className="w-[31px] h-[31px] rounded-full"
+                ></img>
+                <div className="ml-14 text-[13px] mt-12 text-center absolute top-[-80px]">
+                  <div className="text-[#4489FE] text-left">
+                    Vasili Kovanalchuk
+                  </div>
+                  <div className="text-left">
+                    How u doing? Do you wanna
+                    <br /> change some bitcoins?
+                  </div>
+                </div>
+                <div className="text-[12px] text-[#A4AAB3]">18:52</div>
+              </div>
+              <div className=" relative w-full flex justify-end mt-6">
+                <img
+                  src="/public/image/Rectangle 3myblob.svg"
+                  className="w-8/12 "
+                  alt="blob"
+                />
+                <img
+                  src="/public/image/Rectangle 3myblobtail.png"
+                  className="w-[12.3px] h-[17.72px] absolute z-3 bottom-[2px] -right-[4px]"
+                  alt="blob"
+                />
+                <div className="absolute z-5 top-4 text-white text-[13px] w-8/12 px-2 text-left">
+                  Yeah man! I need to change 895 BTC. Do you have it in your
+                  pocket?
+                  <div className="text-white text-[12px] text-right">18:49</div>
+                </div>
+              </div>
+              <div className="flex w-full justify-between relative mt-14">
+                <img
+                  src="/public/image/avatar4.png"
+                  alt=""
+                  className="w-[31px] h-[31px] rounded-full"
+                ></img>
+                <div className="ml-14 text-[13px] mt-12 text-center absolute top-[-80px]">
+                  <div className="text-[#4489FE] text-left">
+                    Vasili Kovanalchuk
+                  </div>
+                  <div className="text-left">
+                    Of course,, I have? And Do you
+                    <br /> have money to buy them?
+                  </div>
+                </div>
+                <div className="text-[12px] text-[#A4AAB3]">18:52</div>
+              </div>
             </TabPanel>
 
             <TabPanel key={"members"} value={"members"}>
