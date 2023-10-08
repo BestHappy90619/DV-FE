5b298ced365f5f4eee9aaea89cb813bfb68ec9cc
AndrijaDev <91664827+AndrijaWBDev@users.noreply.github.com>
2023-10-08 13:10:34 +0900

    update table

--- src/Layouts/FMMiddlePanel.jsx
+++ src/Layouts/FMMiddlePanel.jsx
@@ -2,18 +2,18 @@ import { useEffect, useRef, useState } from "react";
 import { DataGrid } from "@mui/x-data-grid";
 
 const columns = [
-  { field: "id", headerName: "ID", width: 70 },
-  { field: "firstName", headerName: "First name", width: 130 },
-  { field: "lastName", headerName: "Last name", width: 130 },
+  { field: "id", headerName: "", width: 70 },
+  { field: "FileName", headerName: "File Name", width: 130 },
+  { field: "LastUpdated", headerName: "Last Updated", width: 130 },
   {
-    field: "age",
-    headerName: "Age",
+    field: "Size",
+    headerName: "Size",
     type: "number",
     width: 90,
-  },
+  }, // name, length, updated, size
   {
-    field: "fullName",
-    headerName: "Full name",
+    field: "Length",
+    headerName: "Length",
     description: "This column has a value getter and is not sortable.",
     sortable: false,
     width: 160,
@@ -23,24 +23,24 @@ const columns = [
 ];
 
 const rows = [
-  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
-  { id: 10, lastName: "Snow", firstName: "Jon", age: 35 },
-  { id: 11, lastName: "Lannister", firstName: "Cersei", age: 42 },
-  { id: 12, lastName: "Lannister", firstName: "Jaime", age: 45 },
-  { id: 13, lastName: "Stark", firstName: "Arya", age: 16 },
-  { id: 14, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 15, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 16, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 17, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 18, lastName: "Roxie", firstName: "Harvey", age: 65 },
+  { id: 1, FileName: "Snow", LastUpdated: "Jon", Size: 35 },
+  { id: 2, FileName: "Lannister", LastUpdated: "Cersei", Size: 42 },
+  { id: 3, FileName: "Lannister", LastUpdated: "Jaime", Size: 45 },
+  { id: 4, FileName: "Stark", LastUpdated: "Arya", Size: 16 },
+  { id: 5, FileName: "Targaryen", LastUpdated: "Daenerys", Size: null },
+  { id: 6, FileName: "Melisandre", LastUpdated: null, Size: 150 },
+  { id: 7, FileName: "Clifford", LastUpdated: "Ferrara", Size: 44 },
+  { id: 8, FileName: "Frances", LastUpdated: "Rossini", Size: 36 },
+  { id: 9, FileName: "Roxie", LastUpdated: "Harvey", Size: 65 },
+  { id: 10, FileName: "Snow", LastUpdated: "Jon", Size: 35 },
+  { id: 11, FileName: "Lannister", LastUpdated: "Cersei", Size: 42 },
+  { id: 12, FileName: "Lannister", LastUpdated: "Jaime", Size: 45 },
+  { id: 13, FileName: "Stark", LastUpdated: "Arya", Size: 16 },
+  { id: 14, FileName: "Targaryen", LastUpdated: "Daenerys", Size: null },
+  { id: 15, FileName: "Melisandre", LastUpdated: null, Size: 150 },
+  { id: 16, FileName: "Clifford", LastUpdated: "Ferrara", Size: 44 },
+  { id: 17, FileName: "Frances", LastUpdated: "Rossini", Size: 36 },
+  { id: 18, FileName: "Roxie", LastUpdated: "Harvey", Size: 65 },
 ];
 
 const FMMiddlePanel = () => {
@@ -150,7 +150,7 @@ const FMMiddlePanel = () => {
         </div>
       </div>
       <div
-        className={`w-full px-4 flex justify-center overflow-y-auto max-h-[calc(100vh-250px)] `}
+        className={`w-full px-4 flex justify-center overflow-y-auto max-h-[calc(100vh-250px)] font-roboto`}
       >
         <DataGrid
           rows={rows}
