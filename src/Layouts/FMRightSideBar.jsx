aeff21e50e3c2c1abe4bdb43a58f3db898c0aa79
AndrijaDev <91664827+AndrijaWBDev@users.noreply.github.com>
2023-10-10 16:23:15 +0900

    add scrolls to side bars

--- src/Layouts/FMRightSideBar.jsx
+++ src/Layouts/FMRightSideBar.jsx
@@ -21,7 +21,7 @@ const FMRightSideBar = () => {
   ];
 
   return (
-    <div className="flex flex-col  w-full px-2 h-full">
+    <div className="flex flex-col  w-full px-2   h-[calc(100vh-160px)] overflow-y-auto">
       <div className="flex justify-center  w-full">
         <Tabs value={activeTab} className="w-full">
           <TabsHeader
